4cf23feb8ab6152a5ce52dc2f7e3140fdd7fdf51
tyejae <tyejae@gmail.com>
2018-07-11 11:29:26 -0700

    Update ultimus.js

--- commands/raids/ultimus.js
+++ commands/raids/ultimus.js
@@ -19,17 +19,29 @@ class CharacterCmd extends Commando.Command {
         args = args.replace(/  /g, ' ').split(" ");
         if (args.length < 3 || args.length > 3) {
             message.channel.send('You are using this command incorrectly. Use ```!help ultimus``` to learn more.')
-                .then(msg => msg.delete(2000));
+                .then( msg => { msg.delete(2000); message.delete(2000); } )
+                .catch(console.error);
         } else {
             let mention = message.mentions.members.first();
             let arg1 = args[0];
             let arg2 = args[1];
             let arg3 = args[2];
-            return message.channel.send(mention.id)
-                .then( msg => msg.delete(2000) )
-                .catch(console.error);
+            Request.post('https://run.tyejae.com/services/setLane', {
+                id: mention.id,
+                raid: 'ultimus',
+                team: arg2,
+                lane: arg3
+            }, (error, response, body) => {
+                if (!error && response.statusCode == 200) {
+                    return message.channel.send('Assignment set!')
+                        .then( msg => { msg.delete(2000); message.delete(2000); } )
+                        .catch(console.error);
+                } else {
+                    message.channel.send(error)
+                }
+            });
         }
-        message.delete(2000);
+        
         // Request('https://run.tyejae.com/services/getAvailableTeams', function (error, response, body) {
         //     if (!error && response.statusCode == 200) {
         //         // console.log(body) // Print the google web page.
