78f4571e91957a6053712e1e5691ed3ef0035c74
tyejae <tyejae@gmail.com>
2018-07-11 08:17:14 -0700

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Commando = require('discord.js-commando');
 const client = new Commando.Client();
 
-client.login('NDU1MzY4Njk3NTAzMTU0MTg4.DgnxnQ.giYVDSfgkVzCe3quihlA92B4i48');
+client.login(process.env.BOT_TOKEN);
 
 client.registry.registerGroup('msf.gg', 'MSG.GG');
 client.registry.registerGroup('raids', 'raids');
